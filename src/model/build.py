6f4e9c3dd8f5d157db212d94fe4c1c1067111e0b
David Armendariz <david.adrian.a.p@gmail.com>
2024-02-10 10:47:31 -0500

    Use constant for build and train

--- src/model/build.py
+++ src/model/build.py
@@ -6,9 +6,10 @@ from src.Classifier import Classifier
 import os
 import argparse
 import wandb
+from data.constants import project_name
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--IdExecution', type=str, help='ID of the execution')
+parser.add_argument("--IdExecution", type=str, help="ID of the execution")
 args = parser.parse_args()
 
 if args.IdExecution:
@@ -25,16 +26,24 @@ if not os.path.exists("./model"):
 num_classes = 10
 input_shape = 784
 
-def build_model_and_log(config, model, model_name="MLP", model_description="Simple MLP"):
-    with wandb.init(project="MLOps-Pycon2023", 
-        name=f"initialize Model ExecId-{args.IdExecution}", 
-        job_type="initialize-model", config=config) as run:
+
+def build_model_and_log(
+    config, model, model_name="MLP", model_description="Simple MLP"
+):
+    with wandb.init(
+        project=project_name,
+        name=f"initialize Model ExecId-{args.IdExecution}",
+        job_type="initialize-model",
+        config=config,
+    ) as run:
         config = wandb.config
 
         model_artifact = wandb.Artifact(
-            model_name, type="model",
+            model_name,
+            type="model",
             description=model_description,
-            metadata=dict(config))
+            metadata=dict(config),
+        )
 
         name_artifact_model = f"initialized_model_{model_name}.pth"
 
@@ -49,11 +58,13 @@ def build_model_and_log(config, model, model_name="MLP", model_description="Simp
 
 # MLP
 # Testing config
-model_config = {"input_shape":input_shape,
-                "hidden_layer_1": 32,
-                "hidden_layer_2": 64,
-                "num_classes":num_classes}
+model_config = {
+    "input_shape": input_shape,
+    "hidden_layer_1": 32,
+    "hidden_layer_2": 64,
+    "num_classes": num_classes,
+}
 
 model = Classifier(**model_config)
 
-build_model_and_log(model_config, model, "linear","Simple Linear Classifier")
+build_model_and_log(model_config, model, "linear", "Simple Linear Classifier")
