ed105da1e1222258a8c9d24a35cb2a14f0969324
David Armendariz <david.adrian.a.p@gmail.com>
2024-02-10 10:38:14 -0500

    Adds dependencies and fixes things

--- src/data/load.py
+++ src/data/load.py
@@ -1,31 +1,43 @@
 import torch
 import torchvision
 from torch.utils.data import TensorDataset
+
 # Testing
 import argparse
 import wandb
+from constants import project_name
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--IdExecution', type=str, help='ID of the execution')
+parser.add_argument("--IdExecution", type=str, help="ID of the execution")
 args = parser.parse_args()
 
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 
-def load(train_size=.8):
+
+def load(train_size=0.8):
     """
     # Load the data
     """
-      
-    # the data, split between train and test sets
-    train = torchvision.datasets.MNIST(root='./data', train=True, download=True)
-    test = torchvision.datasets.MNIST(root='./data', train=False, download=True)
 
-    (x_train, y_train), (x_test, y_test) = (train.data, train.targets), (test.data, test.targets)
+    # the data, split between train and test sets
+    train = torchvision.datasets.MNIST(root="./data", train=True, download=True)
+    test = torchvision.datasets.MNIST(root="./data", train=False, download=True)
+
+    (x_train, y_train), (x_test, y_test) = (train.data, train.targets), (
+        test.data,
+        test.targets,
+    )
 
     # split off a validation set for hyperparameter tuning
-    x_train, x_val = x_train[:int(len(train)*train_size)], x_train[int(len(train)*train_size):]
-    y_train, y_val = y_train[:int(len(train)*train_size)], y_train[int(len(train)*train_size):]
+    x_train, x_val = (
+        x_train[: int(len(train) * train_size)],
+        x_train[int(len(train) * train_size) :],
+    )
+    y_train, y_val = (
+        y_train[: int(len(train) * train_size)],
+        y_train[int(len(train) * train_size) :],
+    )
 
     training_set = TensorDataset(x_train, y_train)
     validation_set = TensorDataset(x_val, y_val)
@@ -33,21 +45,28 @@ def load(train_size=.8):
     datasets = [training_set, validation_set, test_set]
     return datasets
 
+
 def load_and_log():
     # 🚀 start a run, with a type to label it and a project it can call home
     with wandb.init(
-        project="MLOps-Pycon2023",
-        name=f"Load Raw Data ExecId-{args.IdExecution}", job_type="load-data") as run:
-        
+        project=project_name,
+        name=f"Load Raw Data ExecId-{args.IdExecution}",
+        job_type="load-data",
+    ) as run:
+
         datasets = load()  # separate code for loading the datasets
         names = ["training", "validation", "test"]
 
         # 🏺 create our Artifact
         raw_data = wandb.Artifact(
-            "mnist-raw", type="dataset",
+            "mnist-raw",
+            type="dataset",
             description="raw MNIST dataset, split into train/val/test",
-            metadata={"source": "torchvision.datasets.MNIST",
-                      "sizes": [len(dataset) for dataset in datasets]})
+            metadata={
+                "source": "torchvision.datasets.MNIST",
+                "sizes": [len(dataset) for dataset in datasets],
+            },
+        )
 
         for name, data in zip(names, datasets):
             # 🐣 Store a new file in the artifact, and write something into its contents.
@@ -57,6 +76,7 @@ def load_and_log():
 
         # ✍️ Save the artifact to W&B.
         run.log_artifact(raw_data)
+
 
 # testing
 load_and_log()
--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,20 +1,21 @@
 import torch
-import torchvision
 from torch.utils.data import TensorDataset
 
-#testing
+# testing
 import os
 import argparse
 import wandb
+from constants import project_name
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--IdExecution', type=str, help='ID of the execution')
+parser.add_argument("--IdExecution", type=str, help="ID of the execution")
 args = parser.parse_args()
 
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 else:
     args.IdExecution = "testing console"
+
 
 def preprocess(dataset, normalize=True, expand_dims=True):
     """
@@ -29,23 +30,30 @@ def preprocess(dataset, normalize=True, expand_dims=True):
     if expand_dims:
         # Make sure images have shape (1, 28, 28)
         x = torch.unsqueeze(x, 1)
-    
+
     return TensorDataset(x, y)
+
 
 def preprocess_and_log(steps):
 
-    with wandb.init(project="MLOps-Pycon2023",name=f"Preprocess Data ExecId-{args.IdExecution}", job_type="preprocess-data") as run:    
+    with wandb.init(
+        project=project_name,
+        name=f"Preprocess Data ExecId-{args.IdExecution}",
+        job_type="preprocess-data",
+    ) as run:
         processed_data = wandb.Artifact(
-            "mnist-preprocess", type="dataset",
+            "mnist-preprocess",
+            type="dataset",
             description="Preprocessed MNIST dataset",
-            metadata=steps)
-         
+            metadata=steps,
+        )
+
         # ✔️ declare which artifact we'll be using
-        raw_data_artifact = run.use_artifact('mnist-raw:latest')
+        raw_data_artifact = run.use_artifact("mnist-raw:latest")
 
         # 📥 if need be, download the artifact
         raw_dataset = raw_data_artifact.download(root="./data/artifacts/")
-        
+
         for split in ["training", "validation", "test"]:
             raw_split = read(raw_dataset, split)
             processed_dataset = preprocess(raw_split, **steps)
@@ -56,13 +64,14 @@ def preprocess_and_log(steps):
 
         run.log_artifact(processed_data)
 
+
 def read(data_dir, split):
     filename = split + ".pt"
     x, y = torch.load(os.path.join(data_dir, filename))
 
     return TensorDataset(x, y)
 
-steps = {"normalize": True,
-         "expand_dims": False}
+
+steps = {"normalize": True, "expand_dims": False}
 
 preprocess_and_log(steps)
